e5b848f85db29e021106bc55a070d93a39db350c
ZoharAndroid <1048132071@qq.com>
2017-06-29 11:27:26 +0800

    自定义View：TopBar标题栏
    Signed-off-by: ZoharAndroid <1048132071@qq.com>

--- gen/com/zzh/functiondemo/R.java
+++ gen/com/zzh/functiondemo/R.java
@@ -9,6 +9,79 @@ package com.zzh.functiondemo;
 
 public final class R {
     public static final class attr {
+        /** <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+         */
+        public static final int leftBackground=0x7f010005;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int leftText=0x7f010003;
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int leftTextColor=0x7f010004;
+        /** <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+         */
+        public static final int rightBackground=0x7f010008;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int rightText=0x7f010006;
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int rightTextColor=0x7f010007;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int title=0x7f010000;
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int titleTextColor=0x7f010002;
+        /** <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int titleTextSize=0x7f010001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -24,7 +97,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int topbar=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -65,4 +139,165 @@ public final class R {
          */
         public static final int AppTheme=0x7f060001;
     }
+    public static final class styleable {
+        /**  通过declare-styleable 标签生命了使用自定义属性，并通过name属性来确定引用的名称，最后通过attr
+    标签来声明具体的自定义属性 
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #TopBar_leftBackground com.zzh.functiondemo:leftBackground}</code></td><td></td></tr>
+           <tr><td><code>{@link #TopBar_leftText com.zzh.functiondemo:leftText}</code></td><td></td></tr>
+           <tr><td><code>{@link #TopBar_leftTextColor com.zzh.functiondemo:leftTextColor}</code></td><td></td></tr>
+           <tr><td><code>{@link #TopBar_rightBackground com.zzh.functiondemo:rightBackground}</code></td><td></td></tr>
+           <tr><td><code>{@link #TopBar_rightText com.zzh.functiondemo:rightText}</code></td><td></td></tr>
+           <tr><td><code>{@link #TopBar_rightTextColor com.zzh.functiondemo:rightTextColor}</code></td><td></td></tr>
+           <tr><td><code>{@link #TopBar_title com.zzh.functiondemo:title}</code></td><td></td></tr>
+           <tr><td><code>{@link #TopBar_titleTextColor com.zzh.functiondemo:titleTextColor}</code></td><td></td></tr>
+           <tr><td><code>{@link #TopBar_titleTextSize com.zzh.functiondemo:titleTextSize}</code></td><td></td></tr>
+           </table>
+           @see #TopBar_leftBackground
+           @see #TopBar_leftText
+           @see #TopBar_leftTextColor
+           @see #TopBar_rightBackground
+           @see #TopBar_rightText
+           @see #TopBar_rightTextColor
+           @see #TopBar_title
+           @see #TopBar_titleTextColor
+           @see #TopBar_titleTextSize
+         */
+        public static final int[] TopBar = {
+            0x7f010000, 0x7f010001, 0x7f010002, 0x7f010003,
+            0x7f010004, 0x7f010005, 0x7f010006, 0x7f010007,
+            0x7f010008
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.zzh.functiondemo.R.attr#leftBackground}
+          attribute's value can be found in the {@link #TopBar} array.
+
+
+          <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+          @attr name android:leftBackground
+        */
+        public static final int TopBar_leftBackground = 5;
+        /**
+          <p>This symbol is the offset where the {@link com.zzh.functiondemo.R.attr#leftText}
+          attribute's value can be found in the {@link #TopBar} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:leftText
+        */
+        public static final int TopBar_leftText = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.zzh.functiondemo.R.attr#leftTextColor}
+          attribute's value can be found in the {@link #TopBar} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:leftTextColor
+        */
+        public static final int TopBar_leftTextColor = 4;
+        /**
+          <p>This symbol is the offset where the {@link com.zzh.functiondemo.R.attr#rightBackground}
+          attribute's value can be found in the {@link #TopBar} array.
+
+
+          <p>May be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+<p>May be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+          @attr name android:rightBackground
+        */
+        public static final int TopBar_rightBackground = 8;
+        /**
+          <p>This symbol is the offset where the {@link com.zzh.functiondemo.R.attr#rightText}
+          attribute's value can be found in the {@link #TopBar} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:rightText
+        */
+        public static final int TopBar_rightText = 6;
+        /**
+          <p>This symbol is the offset where the {@link com.zzh.functiondemo.R.attr#rightTextColor}
+          attribute's value can be found in the {@link #TopBar} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:rightTextColor
+        */
+        public static final int TopBar_rightTextColor = 7;
+        /**
+          <p>This symbol is the offset where the {@link com.zzh.functiondemo.R.attr#title}
+          attribute's value can be found in the {@link #TopBar} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:title
+        */
+        public static final int TopBar_title = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.zzh.functiondemo.R.attr#titleTextColor}
+          attribute's value can be found in the {@link #TopBar} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:titleTextColor
+        */
+        public static final int TopBar_titleTextColor = 2;
+        /**
+          <p>This symbol is the offset where the {@link com.zzh.functiondemo.R.attr#titleTextSize}
+          attribute's value can be found in the {@link #TopBar} array.
+
+
+          <p>Must be a dimension value, which is a floating point number appended with a unit such as "<code>14.5sp</code>".
+Available units are: px (pixels), dp (density-independent pixels), sp (scaled pixels based on preferred font size),
+in (inches), mm (millimeters).
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:titleTextSize
+        */
+        public static final int TopBar_titleTextSize = 1;
+    };
 }
--- src/com/zzh/functiondemo/MainActivity.java
+++ src/com/zzh/functiondemo/MainActivity.java
@@ -1,23 +1,50 @@
 package com.zzh.functiondemo;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.Toast;
 
+import com.zzh.functiondemo.view.TopBar;
+import com.zzh.functiondemo.view.TopBar.topbarClickListener;
+
+/**
+ * 
+ * @author zzh
+ * @时间 2017-6-29  上午11:14:51
+ * @描述 TODO
+ */
 public class MainActivity extends Activity {
 
-    @Override
+    private TopBar mTopbar;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        initView();
+        initData();
+       
     }
 
+	private void initData() {
+		 mTopbar.setOnTopbarClickListener(new topbarClickListener() {
+				
+				@Override
+				public void rightClick() {
+					Toast.makeText(MainActivity.this, "返回", 0).show();
+				}
+				
+				@Override
+				public void leftClick() {
+					Toast.makeText(MainActivity.this, "更多", 0).show();
+				}
+			});		
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	private void initView() {
+		 mTopbar = (TopBar) findViewById(R.id.topbar);		
+	}
     
 }
